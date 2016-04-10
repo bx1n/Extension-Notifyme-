1e7f41d25267fb0d1f76dfb8e06b94144903338f
abhinand <abhinand4858@gmail.com>
2016-04-10 21:48:17 +0530

    Adding the notification system

--- NotifyMe.hooks.php
+++ NotifyMe.hooks.php
@@ -3,4 +3,38 @@
 
 class NotifyMeHooks {
 
+	public static function onBeforeCreateEchoEvent( &$notifications, &$notificationCategories )  {
+		$notifications['notifyme-user-notify'] = array(
+		    'category' => 'system',
+		    'section' => 'alert',
+		    'primary-link' => array(
+			'message' => 'notifyme-notification-user-notify',
+			'destination' => 'new-notify'
+		    ),
+		    'formatter-class' => 'EchoNotificationFormatter     ',
+		    'title-message' => 'notifyme-notification-title',
+		    'title-params' => array( 'agent' ),
+		    'flyout-message' => 'notifyme-notification-flyout',
+		    'flyout-params' => array( 'agent' ),
+		    'email-subject-message' => 'notifyme-email-subject',
+		    'email-subject-params' => array( 'agent' ),
+		    'email-body-batch-message' => 'notifyme-email-batch-body',
+		    'email-body-batch-params' =>  array( 'agent' ),
+		);
+		return true;
+
+	}
+
+	public static function onEchoGetDefaultNotifiedUsers( $event, &$users ) {
+		switch ( $event->getType() ) {
+			case 'notifyme-user-notify':
+				$extra = $event->getExtra();
+				$recipientId = $extra['notify-user-id'];
+				$recipient = User::newFromId( $recipientId );
+				$users[$recipientId] = $recipient;
+				break;
+		}
+		return true;
+	}
 }
+
--- specials/SpecialNotifyMe.php
+++ specials/SpecialNotifyMe.php
@@ -2,23 +2,37 @@
 
 class SpecialNotifyMe extends SpecialPage {
 	public function __construct() {
-		parent::__construct( 'NotifyMe' );
+		parent::__construct('NotifyMe');
 	}
 
 
-	public function execute( $sub ) {
+	public function execute($sub) {
 		$out = $this->getOutput();
 
-		$out->setPageTitle( $this->msg( 'notifyme-notify' ) );
+		$out->setPageTitle($this->msg('notifyme-notify'));
 
-		$out->addHelpLink( 'How to become a MediaWiki hacker' );
+		$out->addHelpLink('How to become a MediaWiki hacker');
 		$out->addWikiMsg('notifyme-welcome');
 
 
-		$out->addWikiMsg( 'notifyme-notify-intro' );
+		$out->addWikiMsg('notifyme-notify-intro');
+
+		EchoEvent::create(
+		    array(
+			'type' => 'notifyme-user-notify',
+                        'extra' => array(
+	                        'notify-user-id' => $this ->getUser() ->getId()
+			),
+                        'agent' => $this->getUser(),
+		    )
+		);
+
 	}
 
-	protected function getGroupName() {
-		return 'other';
-	}
+		protected function getGroupName() {
+			return 'other';
+		}
+
 }
+
+
